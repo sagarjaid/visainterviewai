75c98a2e08185d3c6f7444d7832a6844e81fea7e
sagarjaid <69151227+sagarjaid@users.noreply.github.com>
2024-06-17 22:35:22 +0530

    SEO & analytics added

--- components/atoms/SEOMeta.js
+++ components/atoms/SEOMeta.js
@@ -1,68 +1,57 @@
-import Head from 'next/head';
+import Head from "next/head";
 
 const SEOMeta = (props) => {
   const { title, description, slug, imgUrl } = props;
   return (
     <Head>
       <title>{title}</title>
-      <meta
-        name='title'
-        content={title}
-      />
-      <meta
-        name='description'
-        content={description}
-      />
+      <meta name="title" content={title} />
+      <meta name="description" content={description} />
       <link
-        rel='canonical'
-        href={`https://visainterviewai.com/${slug}`}
+        rel="canonical"
+        href={
+          slug
+            ? `https://visainterviewai.com/${slug}`
+            : "https://visainterviewai.com/"
+        }
       />
 
       <meta
-        property='og:type'
-        content='website'
+        name="google-site-verification"
+        content="yqkTsEPyBxAxUcimbxVUySPMRT57sOEfOyQgFQRRcQ0"
       />
+      <meta property="og:type" content="website" />
       <meta
-        property='og:url'
-        content={`https://visainterviewai.com/${slug}`}
+        property="og:url"
+        content={
+          slug
+            ? `https://visainterviewai.com/${slug}`
+            : "https://visainterviewai.com/"
+        }
       />
+      <meta property="og:title" content={title} />
+      <meta property="og:description" content={description} />
       <meta
-        property='og:title'
-        content={title}
-      />
-      <meta
-        property='og:description'
-        content={description}
-      />
-      <meta
-        property='og:image'
-        content={imgUrl || 'https://visainterviewai.com/cover.png'}
+        property="og:image"
+        content={imgUrl || "https://visainterviewai.com/cover.png"}
       />
 
+      <meta property="twitter:card" content="summary_large_image" />
       <meta
-        property='twitter:card'
-        content='summary_large_image'
+        property="twitter:url"
+        content={
+          slug
+            ? `https://visainterviewai.com/${slug}`
+            : "https://visainterviewai.com/"
+        }
       />
+      <meta property="twitter:title" content={title} />
+      <meta property="twitter:description" content={description} />
       <meta
-        property='twitter:url'
-        content={`https://visainterviewai.com/${slug}`}
+        property="twitter:image"
+        content={imgUrl || "https://visainterviewai.com/cover.png"}
       />
-      <meta
-        property='twitter:title'
-        content={title}
-      />
-      <meta
-        property='twitter:description'
-        content={description}
-      />
-      <meta
-        property='twitter:image'
-        content={imgUrl || 'https://visainterviewai.com/cover.png'}
-      />
-      <link
-        rel='icon'
-        href='https://visainterviewai.com/favicon.svg'
-      />
+      <link rel="icon" href="https://visainterviewai.com/favicon.svg" />
     </Head>
   );
 };
--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,38 @@
-import '@/styles/globals.css';
+import "@/styles/globals.css";
+import Script from "next/script";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Script
+        src={`https://www.googletagmanager.com/gtag/js?id=G-HJN7SJ6V09`}
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){window.dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', 'G-HJN7SJ6V09');
+      `}
+      </Script>
+      <Component {...pageProps} />
+      <Script
+        strategy="afterInteractive"
+        onLoad={() => {
+          console.log("hjar");
+        }}
+        dangerouslySetInnerHTML={{
+          __html: ` (function(h,o,t,j,a,r){
+        h.hj = h.hj || function () { (h.hj.q = h.hj.q || []).push(arguments) };
+      h._hjSettings={hjid:5027212,hjsv:6};
+      a=o.getElementsByTagName('head')[0];
+      r=o.createElement('script');r.async=1;
+      r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+      a.appendChild(r);
+    })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');`,
+        }}
+      />
+    </>
+  );
 }
